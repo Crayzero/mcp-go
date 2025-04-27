21a1364d1ff9c9c8ff8577dece0544c18aeb922c
elricchen <elricchen@tencent.com>
2025-04-27 16:46:02 +0800

    feat(close_on_other.go): :sparkles: juse send SIGKILL in linux

--- client/transport/close_on_other.go
+++ client/transport/close_on_other.go
@@ -4,35 +4,13 @@
 package transport
 
 import (
-	"errors"
-	"fmt"
 	"os"
 	"os/exec"
-	"strings"
 	"syscall"
-	"time"
 )
 
 // killprocess kills the process on non-windows platforms.
 func killProcess(proc *os.Process) error {
-	err := proc.Signal(syscall.SIGTERM)
-	if err != nil {
-		fmt.Printf("Failed to send SIGTERM to pid %d: %v\n", proc.Pid, err)
-	}
-	// wait for a short time to allow the process to terminate gracefully
-	time.Sleep(200 * time.Millisecond)
-	// check if the process is still running
-	if err := proc.Signal(syscall.Signal(0)); err != nil {
-		if errors.Is(err, os.ErrProcessDone) {
-			return nil
-		}
-		// check if the process is gone
-		// on some platforms, this may return "no such process" if the process is already gone
-		if strings.Contains(err.Error(), "no such process") {
-			return nil
-		}
-	}
-
 	// if the process is still running, kill it
 	return syscall.Kill(-proc.Pid, syscall.SIGKILL)
 }
